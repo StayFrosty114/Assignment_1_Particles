2bbe6d356c649d42fa8c991d4bf60f52167a22de
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-04-16 19:03:21 -0700

    Removed Start/Update function in SC_Spellbook.cs and replaced them with a SpellInfo function called when toggles are activated

--- Assets/Scripts/SC_SpellBook.cs
+++ Assets/Scripts/SC_SpellBook.cs
@@ -17,18 +17,9 @@ public class SC_SpellBook : MonoBehaviour
 
 
    
-    void Start()
+    public void SpellInfo()
     {
         // Setting the UI text equal to the spellData values.
-        nameText.text = spellData.spellName;
-        drainText.text = spellData.drain;
-        cooldownText.text = spellData.cooldown;
-        descText.text = spellData.description;
-    }
-
-    private void Update()
-    {
-        // Refreshing UI text every frame. (To fix a problem with text not updating when activating a spell that had already been activated).
         nameText.text = spellData.spellName;
         drainText.text = spellData.drain;
         cooldownText.text = spellData.cooldown;
